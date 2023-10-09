28c017cb6a7afe788306e4cfd288b5342a50250e
iceice666 <iceice666@outlook.com>
2023-10-09 23:41:52 +0800

    setup env

--- src/client/java/net/iceice666/ModClient.java
+++ src/client/java/net/iceice666/ModClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package net.iceice666;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.

758ba70c792320ddc943d938c58345d95399a782
iceice666 <iceice666@outlook.com>
2023-10-10 11:32:10 +0800

    MailCommand Command template

--- src/main/java/net/iceice666/Command/MailCommand.java
+++ src/main/java/net/iceice666/Command/MailCommand.java
@@ -12,13 +12,57 @@ import java.util.UUID;
 
 public final class MailCommand {
 
-    // It contains all created mail
-    private HashMap<UUID, Mail> MailList = new HashMap<>();
+
+    // It contains all created mails
+    private HashMap<UUID, Mail> mailList = new HashMap<>();
+
+    // It contains Players' mail that created
+    private HashMap</*Player*/UUID, /*Mail*/UUID> createdList = new HashMap<>();
+
+    // It contains Players' mail that be sent
+    private HashMap</*Player*/UUID, /*Mail*/UUID> sentList = new HashMap<>();
+
+    // It contains Players' mail that received
+    private HashMap</*Player*/UUID, /*Mail*/UUID> receivedList = new HashMap<>();
+
+    // It contains Players' mail that in trash can
+    private HashMap</*Player*/UUID, /*Mail*/UUID> trashList = new HashMap<>();
+
 
     // This function defines your command
+    /*
+     * Commands
+     * /mail
+     * -> Show info
+     * /mail help
+     * -> Show help
+     * /mail list <Choice created|sent|received|trashcan>
+     * -> Show list with given category
+     * /mail create [String title] [String body] [Boolean attachOffhand]
+     * -> Create a new mail with title
+     * /mail update <Choice {createdList}> <Choice title|body> <String newValue>
+     * /mail update <Choice {createdList}> <Choice attachment> add
+     * /mail update <Choice {createdList}> <Choice attachment> remove
+     * -> Update a created mail field with new value
+     * /mail send <Choice {createdList}> <UUID PlayerUuid>
+     * -> Send a mail to a player
+     * /mail unsend <Choice {sentList}>
+     * -> Unsend a mail in sentList
+     * /mail remove <Choice created|received> <Choice {listItems}>
+     * -> Move a mail to trashcan
+     * /mail revert <Choice {trashList}>
+     * -> Move a mail in trashcan back to created/received list
+     * /mail clear
+     * -> Clear trashcan
+     *  */
+
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
     }
 
+
+    private static int executeList(ServerCommandSource source) throws CommandSyntaxException {
+        return Command.SINGLE_SUCCESS;
+    }
     // This function create a new mail
     private static int executeCreate(ServerCommandSource source) throws CommandSyntaxException {
         return Command.SINGLE_SUCCESS;
@@ -39,7 +83,7 @@ public final class MailCommand {
         return Command.SINGLE_SUCCESS;
     }
 
-    private static int executeRevert(ServerCommandSource source) throws CommandSyntaxException {
+    private static int executeUnsend(ServerCommandSource source) throws CommandSyntaxException {
         return Command.SINGLE_SUCCESS;
     }
 
